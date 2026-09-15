872e3f77a422877007d1a15978ea0d699f807d6e
glenannang <angelaannang8@gmail.com>
2026-09-15 23:25:56 +0800

    feat: add get room status endpoint

--- src/main/java/com/svi/tictactoe/service/RoomService.java
+++ src/main/java/com/svi/tictactoe/service/RoomService.java
@@ -3,9 +3,11 @@ package com.svi.tictactoe.service;
 import com.svi.tictactoe.dto.request.CreateRoomRequest;
 import com.svi.tictactoe.dto.request.JoinRoomRequest;
 import com.svi.tictactoe.dto.response.RoomResponse;
+import com.svi.tictactoe.dto.response.RoomStatusResponse;
 
 public interface RoomService {
 
     RoomResponse joinRoom(String roomCode, JoinRoomRequest request);
     RoomResponse createRoom(String roomCode, CreateRoomRequest request);
+    RoomStatusResponse getRoomStatus(String roomCode);
 }
