15c60efce6086e29be23fb4d5ec960d7da678154
glenannang <angelaannang8@gmail.com>
2026-09-16 17:16:04 +0800

    feat: created add moves dto request and response

--- src/main/java/com/svi/tictactoe/dto/request/AddMoveRequest.java
+++ src/main/java/com/svi/tictactoe/dto/request/AddMoveRequest.java
@@ -1,4 +1,34 @@
 package com.svi.tictactoe.dto.request;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
 public class AddMoveRequest {
+
+    @NotNull(message = "Player ID is required.")
+    private UUID playerId;
+
+    @Min(value = 0, message = "Position must be between 0 and 8.")
+    @Max(value = 8, message = "Position must be between 0 and 8.")
+    private int position;
+
+
+    public UUID getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(UUID playerId) {
+        this.playerId = playerId;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
 }
--- src/main/java/com/svi/tictactoe/dto/response/AddMoveResponse.java
+++ src/main/java/com/svi/tictactoe/dto/response/AddMoveResponse.java
@@ -1,4 +1,23 @@
 package com.svi.tictactoe.dto.response;
 
 public class AddMoveResponse {
+
+    String message;
+    int moveNumber;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getMoveNumber() {
+        return moveNumber;
+    }
+
+    public void setMoveNumber(int moveNumber) {
+        this.moveNumber = moveNumber;
+    }
 }
