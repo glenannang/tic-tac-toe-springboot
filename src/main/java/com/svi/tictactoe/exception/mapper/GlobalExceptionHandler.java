f183ab160fb20b65a15145da4bb8e2210a9e1fb1
glenannang <angelaannang8@gmail.com>
2026-09-17 23:30:31 +0800

    feat: add global exception handling

--- src/main/java/com/svi/tictactoe/exception/mapper/GlobalExceptionHandler.java
+++ src/main/java/com/svi/tictactoe/exception/mapper/GlobalExceptionHandler.java
@@ -1,8 +1,9 @@
 package com.svi.tictactoe.exception.mapper;
 
-import com.svi.tictactoe.exception.RoomDoesNotExistException;
+import com.svi.tictactoe.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,11 +11,47 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(RoomDoesNotExistException.class)
-    public ResponseEntity<String> handleRoomDoesNotExist(RoomDoesNotExistException exception) {
+    @ExceptionHandler({
+            RoomDoesNotExistException.class,
+            GameDoesNotExistException.class,
+            PlayerDoesNotExistException.class
+    })
+    public ResponseEntity<String> handleNotFound(RuntimeException exception) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
     }
 
+
+    @ExceptionHandler({
+            GameAlreadyFinishedException.class,
+            InvalidTurnException.class,
+            PlayerAlreadyInRoomException.class,
+            PositionAlreadyTakenException.class,
+            RoomAlreadyExistsException.class,
+            RoomUnavailableException.class
+    })
+    public ResponseEntity<String> handleConflict(RuntimeException exception) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
+    }
+
+    @ExceptionHandler({
+            PlayerNotInGameException.class,
+            PlayerNotInRoomException.class
+    })
+
+    public ResponseEntity<String> handleForbidden(RuntimeException exception) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationErrors(MethodArgumentNotValidException exception) {
+
+        String message = exception.getBindingResult()
+                .getFieldErrors()
+                .getFirst()
+                .getDefaultMessage();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
+    }
 
 
 
