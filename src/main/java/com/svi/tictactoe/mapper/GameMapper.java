640b2a086939d030f84da658ed0d829c2e192baa
glenannang <angelaannang8@gmail.com>
2026-09-17 10:37:35 +0800

    feat: add game status response and mapper

--- src/main/java/com/svi/tictactoe/mapper/GameMapper.java
+++ src/main/java/com/svi/tictactoe/mapper/GameMapper.java
@@ -1,8 +1,12 @@
 package com.svi.tictactoe.mapper;
 
 import com.svi.tictactoe.dto.response.CreateGameResponse;
+import com.svi.tictactoe.dto.response.GameStatusResponse;
 import com.svi.tictactoe.entity.Game;
+import com.svi.tictactoe.enums.PlayerSymbol;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class GameMapper {
@@ -16,4 +20,23 @@ public class GameMapper {
 
         return response;
     }
+
+    public GameStatusResponse toGameStatusResponse(Game game, List<PlayerSymbol> board, PlayerSymbol nextTurn, int moveCount) {
+
+        GameStatusResponse response = new GameStatusResponse();
+
+        response.setGameId(game.getGameId());
+        response.setStatus(game.getStatus());
+        response.setBoard(board);
+        response.setNextTurn(nextTurn);
+        response.setMoveCount(moveCount);
+        response.setWinnerId(game.getWinnerId());
+        response.setResult(game.getResult());
+
+        return response;
+    }
+
+
+
+
 }
