6aeed10a847fc32bc746badbe1bafae2bd33a9f7
glenannang <angelaannang8@gmail.com>
2026-09-18 00:13:20 +0800

    feat: add leaderboard response DTOs

---
+++ src/main/java/com/svi/tictactoe/dto/response/LeaderboardEntryResponse.java
@@ -0,0 +1,70 @@
+package com.svi.tictactoe.dto.response;
+
+import java.util.UUID;
+
+public class LeaderboardEntryResponse {
+
+    private int rank;
+    private UUID playerId;
+    private int wins;
+    private int losses;
+    private int draws;
+    private int gamesPlayed;
+    private int incompleteGames;
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public UUID getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(UUID playerId) {
+        this.playerId = playerId;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+
+    public void setLosses(int losses) {
+        this.losses = losses;
+    }
+
+    public int getDraws() {
+        return draws;
+    }
+
+    public void setDraws(int draws) {
+        this.draws = draws;
+    }
+
+    public int getGamesPlayed() {
+        return gamesPlayed;
+    }
+
+    public void setGamesPlayed(int gamesPlayed) {
+        this.gamesPlayed = gamesPlayed;
+    }
+
+    public int getIncompleteGames() {
+        return incompleteGames;
+    }
+
+    public void setIncompleteGames(int incompleteGames) {
+        this.incompleteGames = incompleteGames;
+    }
+}
